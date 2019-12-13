0a6553d622055e01e6c4bb3643094bac087910a3
HectorJC <jimcont.hector@gmail.com>
2019-12-13 00:54:58 -0600

    Mi primer script en Pyton :D

--- semana1/script01_print.py
+++ semana1/script01_print.py
@@ -1,3 +1,8 @@
-texto = 'Hello World!'
-print('texto')
-print(texto)
+# texto = 'Hello World!'
+# print('texto')
+# print(texto)
+
+# Mi script Héctor Jiménez Contreras
+
+mensajito = 'Hola a todos'
+print(mensajito)
