2bd983bd78cc8c979b6e2ea737fd8f36dd096637
HectorJC <jimcont.hector@gmail.com>
2019-12-13 01:11:06 -0600

    Tarea de funciones

---
+++ scriptsPython/miSemana1/tareas/funciones.py
@@ -0,0 +1,17 @@
+def es_trinagulo_cuadrado(n1, n2):
+    if n1 == n2:
+        return 'Es cuadrado'
+    else:
+        return 'Es Triangulo'
+
+
+print(es_trinagulo_cuadrado(1, 3))
+print(es_trinagulo_cuadrado(3, 3))
+print(es_trinagulo_cuadrado(2, 1))
+print(es_trinagulo_cuadrado(1, 23))
+print(es_trinagulo_cuadrado(2, 13))
+print(es_trinagulo_cuadrado(1, 34))
+print(es_trinagulo_cuadrado(34, 12))
+print(es_trinagulo_cuadrado(2, 4))
+print(es_trinagulo_cuadrado(5, 9))
+print(es_trinagulo_cuadrado(6, 9))
